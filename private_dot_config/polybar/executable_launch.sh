04ff235b782c952c9e95c9349ec1b9c9dea95512
madlabman <10616301+madlabman@users.noreply.github.com>
2022-12-03 20:12:44 +0100

    chore: sync dotfiles

--- private_dot_config/polybar/executable_launch.sh
+++ private_dot_config/polybar/executable_launch.sh
@@ -9,8 +9,7 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch Polybar, using default config location ~/.config/polybar/config
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar -c $HOME/.config/polybar/config.ini --reload top &
-    MONITOR=$m polybar -c $HOME/.config/polybar/config.ini --reload bottom &
+    MONITOR=$m polybar -c $HOME/.config/polybar/config.ini --reload main
   done
 else
   echo "Lake of xrandr support"
