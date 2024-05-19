e83eb8235e0da483654b8300a6471d5462394995
madlabman <10616301+madlabman@users.noreply.github.com>
2024-05-19 17:25:44 +0300

    feat: Justfile support

--- private_dot_config/nvim/lua/user/aucommands.lua
+++ private_dot_config/nvim/lua/user/aucommands.lua
@@ -10,6 +10,12 @@ vim.api.nvim_create_autocmd("BufEnter", {
 	command = "set filetype=vyper",
 })
 
+-- recognize just files
+vim.api.nvim_create_autocmd("BufEnter", {
+	pattern = { "[Jj]ustfile" },
+	command = "setlocal filetype=just nolinebreak",
+})
+
 -- commentstring for solidity
 vim.api.nvim_create_autocmd("BufEnter", {
 	pattern = { "*.sol" },
