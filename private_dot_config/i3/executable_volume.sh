5161a53e6977d5b964783ea928ae3e134e47d387
madlabman <10616301+madlabman@users.noreply.github.com>
2025-10-12 09:59:45 +0200

    chore: upd i3 config

--- private_dot_config/i3/executable_volume.sh
+++ private_dot_config/i3/executable_volume.sh
@@ -2,21 +2,27 @@
 
 set -eux
 
-SINK="@DEFAULT_SINK@"
-SET_SINK_VOLUME="pactl set-sink-volume $SINK"
-SET_SINK_MUTE="pactl set-sink-mute $SINK"
+SINK="@DEFAULT_AUDIO_SINK@"
+SET_SINK_VOLUME="wpctl set-volume -l 1.5 $SINK"
+SET_SINK_MUTE="wpctl set-mute $SINK"
+SOURCE="@DEFAULT_AUDIO_SOURCE@"
+SET_SOURCE_MUTE="wpctl set-mute $SOURCE"
 
 case $1 in
 	up)
-		$SET_SINK_VOLUME +5%
+		$SET_SINK_VOLUME 5%+
 		;;
 
 	down)
-		$SET_SINK_VOLUME -5%
+		$SET_SINK_VOLUME 5%-
 		;;
 
 	toggle)
 		$SET_SINK_MUTE toggle
+		;;
+
+	mic)
+		$SET_SOURCE_MUTE toggle
 		;;
 
 	max)
